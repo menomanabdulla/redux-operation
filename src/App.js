32eeb811cff9c2ba5b07e0af55e73cd6bdac2c3d
nomanabdulla <noman.fcibappy@gmail.com>
2018-10-29 03:25:56 +0600

    Redux project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import {createStore} from 'redux'
 
 class App extends Component {
   render() {
+    /*const reducer = (state = {},action)=>{
+      if(action.type === 'A'){
+        return{
+          ...state,
+          A: 'I am A'
+        }
+      }
+      if(action.type === 'B'){
+        return{
+          ...state,
+          B: 'I am B'
+        }
+      }
+      return state;
+    }
+    const store = createStore(reducer);
+    store.subscribe(()=>{
+      console.log(store.getState());
+    })
+
+    store.dispatch({type: "A"})
+    store.dispatch({type: "Something"})
+    store.dispatch({type: "B"})
+    */
+
+    const reducer = (state={},action)=>{
+      return state;
+    }
+    const store = createStore(reducer);
+
+    store.subscribe(()=>{
+      console.log(store.getState());
+    })
+
+    store.dispatch({type: 'Something'})
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
